ccd0d0c9cd4b8aed00f608b2d1252e9229c79095
tanakaderoy <tanaka.deroy@gmail.com>
2019-01-22 13:59:56 -0500

    Update

--- Assignment 2/Assignment 2/main.swift
+++ Assignment 2/Assignment 2/main.swift
@@ -120,37 +120,36 @@
  Base Total: $11.50
  
  For the program output, use the description above, as well as clearly labeled values for serviceLevel and finalTotal, to inform the user of the entire bill for each test case. For serviceLevel, show the value in percent and dollar amount. Have at least 6 test cases, where you probe edge conditions, such as an empty bill, or unreasonable values (-3 salads, 0 hamburgers at $4.99 each, a pickle that costs -0.25)
- */
+ *///
+//  main.swift
+//  Assignment 2
+//
+//  Created by tanaka on 1/15/19.
+//  Copyright © 2019 tanaka. All rights reserved.
+//
 import Foundation
 
-
-print("Problem 1: ")
-
-var rangeList =  [9,37,900,736,40,50]
-for rangeNum in 0..<rangeList.count {
-    if rangeList[rangeNum] > 50 {
-        print ("Range-Style Loop: \(rangeList[rangeNum])")
-}
-}
-
-
-var list = [1,50,566,764,12,33]
-for num in list{
-    if (num > 50){
-        print("For in loop: \(num)")
-    }
-}
-
-let fiveList = [10,20,50,51,70,90]
-var index = 0
-while index < fiveList.count {
-    if (fiveList[index] > 50){
-        print("While loop: \(fiveList[index])")
-        
-        
-    }
-    index += 1
-}
+/*
+ print("Hello, World!")
+ 
+ 
+ var list = [1,50,566,764,12,33]
+ for num in list{
+ if (num > 50){
+ print(num)
+ }
+ }
+ 
+ let fiveList = [10,20,50,51,70,90]
+ var index = 0
+ while index < fiveList.count {
+ if (fiveList[index] > 50){
+ print(fiveList[index])
+ 
+ 
+ }
+ index += 1
+ }*/
 
 //problem 2
 print("Problem 2: ")
@@ -158,25 +157,64 @@ func englishWordsOfValues(_ numbers: [Int]) {
     let textNum = [0: "zero",1: "one", 2: "two", 3: "three",4: "four",5: "five",6: "six",7: "seven",8: "eight",9: "nine"]
     let tens = [0: " ",10: "ten", 20: "twenty", 30: "thirty", 40: "fourty", 50: "fifty", 60: "sixty",70: "seventy", 80: "eighty", 90: "ninety"]
     var index = 0
-    if (numbers != []){
+    
     while index < numbers.count {
+        if (numbers[index] >= 100 || numbers.isEmpty ){
+            print("\(numbers[index]): not valid")
+        }
         for word in textNum{
             
             
             let onesDigit = numbers[index] % 10
-            
             if( onesDigit == word.key){
                 let tensDigit = numbers[index] - onesDigit
                 for digit in tens {
                     
                     if (tensDigit == digit.key){
-                        if numbers[index] == 10 || numbers[index] == 20 || numbers[index] == 30 || numbers[index] == 40 || numbers[index] == 50 || numbers[index] == 60 || numbers[index] == 70 || numbers[index] == 80 || numbers[index] == 90 {
+                        
+                        
+                        switch numbers[index] {
+                        case 10 :
                             print("\(numbers[index]) in english is \(digit.value)")
-                        }else{
-                            print("\(numbers[index]) in english is: \(digit.value) \(word.value) ")
                             
+                        case 20:
+                            print("\(numbers[index]) in english is \(digit.value)")
+                        case 30:
+                            print("\(numbers[index]) in english is \(digit.value)")
+                        case 40:
+                            print("\(numbers[index]) in english is \(digit.value)")
+                        case 50:
+                            print("\(numbers[index]) in english is \(digit.value)")
+                        case 60:
+                            print("\(numbers[index]) in english is \(digit.value)")
+                        case 70:
+                            print("\(numbers[index]) in english is \(digit.value)")
+                        case 80:
+                            print("\(numbers[index]) in english is \(digit.value)")
+                        case 90:
+                            print("\(numbers[index]) in english is \(digit.value)")
+                        case 11:
+                            print("\(numbers[index]) in english is eleven")
+                        case 12:
+                            print("\(numbers[index]) in english is twelve")
+                        case 13:
+                            print("\(numbers[index]) in english is thirteen")
+                        case 14:
+                            print("\(numbers[index]) in english is fourteen")
+                        case 15:
+                            print("\(numbers[index]) in english is fifteen")
+                        case 16..<20:
+                            print("\(numbers[index]) in english is \(word.value)teen")
+                        default:
+                            print("\(numbers[index]) in english is \(digit.value) \(word.value)")
                         }
-                        print("empty")
+                        
+                        /*if numbers[index] == 10 || numbers[index] == 20 || numbers[index] == 30 || numbers[index] == 40 || numbers[index] == 50 || numbers[index] == 60 || numbers[index] == 70 || numbers[index] == 80 || numbers[index] == 90 {
+                         print("\(numbers[index]) in english is \(digit.value)")
+                         }else{
+                         print("\(numbers[index]) in english is: \(digit.value) \(word.value) ")
+                         
+                         }*/
                     }
                 }
             }
@@ -194,8 +232,7 @@ func englishWordsOfValues(_ numbers: [Int]) {
     }
     
 }
-    print("Empty Set")
-}
+
 
 
 
@@ -204,4 +241,4 @@ func englishWordsOfValues(_ numbers: [Int]) {
 /*englishWordsOfValues(fiveList)
  englishWordsOfValues(makeList(10))
  */
-englishWordsOfValues([1,2])
+englishWordsOfValues([1,2,33,99,10,-1,100,19,90])
